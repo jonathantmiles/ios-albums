3950b4f3fcaab6be704d361284a00a8b540e6c2d
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-31 15:24:10 -0700

    lays out storyboards

---
+++ Albums/Albums/Cells/SongTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Jonathan T. Miles on 8/31/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBAction func addSong(_ sender: Any) {
+    
+    }
+    
+    
+    @IBOutlet weak var songTitleTextField: UITextField!
+    
+    @IBOutlet weak var songDurationTextField: UITextField!
+    
+}
